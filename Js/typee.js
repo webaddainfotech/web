20bf59cab934a9839315be1fe430f3853c8ef7b4
webaddainfotech <94195892+webaddainfotech@users.noreply.github.com>
2022-01-16 15:17:05 +0530

    Add files via upload

---
+++ Js/typee.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.element', {
+    strings: ["Web Development.", "Website Design.", "Logo Design.", "Infographic Design","WhatsApp Sender Software.","Whatsapp sender- Excel."],
+    typeSpeed: 100,
+    loop:true,
+   startDelay:1000,
+    });
